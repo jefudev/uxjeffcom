6ec158b680ccda0098058b55023644fbe862ad07
jefudev <jefl2109@gmail.com>
2021-09-09 13:57:31 -0600

    updated background height

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div class="bg-gray-600 h-full">
+  <div class="bg-gray-600 h-screen min-h-full">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
       <div class="max-w-3xl mx-auto py-16">
